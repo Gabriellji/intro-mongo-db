cb6ed06181cfe67638332991786a57db790f6f1f
Gabriellji <gabriellji@yahoo.com>
2021-01-31 18:09:54 +0300

    fix: user model + mongo crud methods

--- exercises/connect.js
+++ exercises/connect.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 
-const connect = (url) => Promise.reject()
+
+const connect = (url) => mongoose.connect(url, {
+    useNewUrlParser: true,
+    poolSize: 10, 
+    bufferMaxEntries: 0, 
+    reconnectTries: 5000,
+    useUnifiedTopology: true
+  })
+  
 
 module.exports = connect
+
